ecbfcccf9d508669b7f2fe5188829b90dec02fb1
amiish <amiishadhool979@gmail.com>
2024-08-31 11:14:24 +0300

    Done

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
 import express,{Express,Request,Response} from 'express';
 const app:Express = express();
-const port:number = 4321
+const port:number = 4321;
 
-const fariin:string = "waan ku salamay walaal"
-const anotherMessage:string = "I like you"
+
+const personInfo={
+    id:1,
+    name:'Amiisha',
+    isStudent: false,
+}
+
 app.get('/',(req:Request,res:Response)=>{
     res.json({
         message: "Hello Mrs Amiisha Laki welcome to TypeScript",
-        status: 'You are Legend',
-        fariin,
-        anotherMessage
-
+        personInfo
     })
 })
 
