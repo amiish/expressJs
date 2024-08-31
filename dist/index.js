e881dee0770c412b4f7f7e72c39e8fe1ac2126cd
amiish <amiishadhool979@gmail.com>
2024-08-31 11:16:57 +0300

    Done:

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 const app = express();
 const port = 4321;
-const fariin = "waan ku salamay walaal";
-const anotherMessage = "I like you";
+const personInfo = {
+    id: 1,
+    name: 'Amiisha',
+    isStudent: false,
+};
 app.get('/', (req, res) => {
     res.json({
         message: "Hello Mrs Amiisha Laki welcome to TypeScript",
-        status: 'You are Legend',
-        fariin
+        personInfo
     });
 });
 app.listen(port, () => {
